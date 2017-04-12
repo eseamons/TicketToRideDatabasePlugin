a5436b34dc932c2b92c0900442096e778487fcc0
bexredd <r.redd94@gmail.com>
2017-04-11 21:02:52 -0600

    YAY!

--- src/plugin/SQLiteFactory.java
+++ src/plugin/SQLiteFactory.java
@@ -1,5 +1,29 @@
 package plugin;
 
-public class SQLiteFactory {
+import server.plugin.IAccountDao;
+import server.plugin.ICommandDao;
+import server.plugin.IDaoFactory;
+import server.plugin.IGameDao;
 
+public class SQLiteFactory implements IDaoFactory{
+
+	@Override
+	public IAccountDao createAccountDao() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ICommandDao createCommandDao() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IGameDao createGameDao() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 }
--- src/plugin/SerializeFactory.java
+++ src/plugin/SerializeFactory.java
@@ -1,5 +1,28 @@
 package plugin;
 
-public class SerializeFactory {
+import server.plugin.IAccountDao;
+import server.plugin.ICommandDao;
+import server.plugin.IDaoFactory;
+import server.plugin.IGameDao;
+
+public class SerializeFactory implements IDaoFactory {
+
+	@Override
+	public IAccountDao createAccountDao() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ICommandDao createCommandDao() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IGameDao createGameDao() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
