5b1f18039aadb5a83115b0a05b3645c4b2a3f4c2
eseamons <ericseamons90@gmail.com>
2017-04-12 16:16:09 -0600

    Added Database class to plugin package

---
+++ src/plugin/Database.java
@@ -0,0 +1,124 @@
+package plugin;
+
+import java.rmi.ServerException;
+import java.sql.*;
+
+
+public class Database {
+
+		private static final String DATABASE_FILE = "database.sqlite";
+		private static final String DATABASE_URL = "jdbc:sqlite:" + DATABASE_FILE;
+
+		public static void initialize() throws ServerException {
+			try {
+				final String driver = "org.sqlite.JDBC";
+				Class.forName(driver);
+				
+			}
+			catch(ClassNotFoundException e) {
+				throw new ServerException("Could not load database driver", e);
+			}
+		}
+
+
+		
+		
+		
+		
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+		private Connection connection;
+		
+		
+		public Connection getConnection() {
+			return connection;
+		}
+
+		public void startTransaction() throws ServerException {
+			try {
+				assert (connection == null);			
+				connection = DriverManager.getConnection(DATABASE_URL);
+				connection.setAutoCommit(false);
+			}
+			catch (SQLException e) {
+				throw new ServerException("Could not connect to database. Make sure " + 
+					DATABASE_FILE, e);
+			}
+		}
+		
+		public boolean inTransaction() {
+			return (connection != null);
+		}
+		
+		public void endTransaction(boolean commit) {
+			if (connection != null) {		
+				try {
+					if (commit) {
+						connection.commit();
+					}
+					else {
+						connection.rollback();
+					}
+				}
+				catch (SQLException e) {
+					System.out.println("Could not end transaction");
+					e.printStackTrace();
+				}
+				finally {
+					safeClose(connection);
+					connection = null;
+				}
+			}
+		}
+		
+		public static void safeClose(Connection conn) {
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					// ...
+				}
+			}
+		}
+		
+		public static void safeClose(Statement stmt) {
+			if (stmt != null) {
+				try {
+					stmt.close();
+				}
+				catch (SQLException e) {
+					// ...
+				}
+			}
+		}
+		
+		public static void safeClose(PreparedStatement stmt) {
+			if (stmt != null) {
+				try {
+					stmt.close();
+				}
+				catch (SQLException e) {
+					// ...
+				}
+			}
+		}
+		
+		public static void safeClose(ResultSet rs) {
+			if (rs != null) {
+				try {
+					rs.close();
+				}
+				catch (SQLException e) {
+					// ...
+				}
+			}
+		}
+		
+		
+		public static void createDatabaseTables(){
+			
+		}
+		
+}
+
+
