cb855ec429daa640f2644aa6fe28d6039463256e
lkjames <thelancejames@gmail.com>
2017-04-16 03:14:09 -0600

    fixed merge problem

--- src/main/TicketToRidePlugin.java
+++ src/main/TicketToRidePlugin.java
@@ -1,7 +1,39 @@
 package main;
 
+import java.util.Set;
+
+import plugin.JSONFactory;
+import server.plugin.AccountDTO;
+import server.plugin.IAccountDao;
+import shared.model_classes.Account;
+
 public class TicketToRidePlugin {
 	public static void main(String[] args) {
 		
+		/*
+		 * LANCE'S TEST CODE
+		 */
+//		JSONFactory factory = new JSONFactory();
+//		IAccountDao accountDAO = factory.createAccountDao();
+//		
+//		AccountDTO dto = new AccountDTO();
+//		Account account = new Account();
+//		account.setAuthentication("HELLO-12345-THEREX");
+//		account.setUsername("master");
+//		account.setPassword("power overwhelming");
+//		dto.setAccount(account);
+//		dto.setGameID(1);
+//		
+//		accountDAO.addAccount(dto);
+//		Set<AccountDTO> list = accountDAO.getAll();
+//		
+//		for(AccountDTO accountDTO : list){
+//			System.out.println(accountDTO.getAccount().getAuthentication());
+//			System.out.println(accountDTO.getAccount().getUsername());
+//			System.out.println(accountDTO.getAccount().getPassword());
+//			System.out.println(accountDTO.getGameID());
+//		}
+//		
+//		accountDAO.clearData();
 	}
 }
--- src/plugin/JSONAccountDao.java
+++ src/plugin/JSONAccountDao.java
@@ -1,50 +1,201 @@
 package plugin;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
+import java.io.IOException;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.cedarsoftware.util.io.JsonReader;
 import com.cedarsoftware.util.io.JsonWriter;
 
 import server.plugin.AccountDTO;
 import server.plugin.IAccountDao;
 import shared.Result;
+import shared.model_classes.Account;
 
 public class JSONAccountDao implements IAccountDao {
+	
+	private String path = "JSONdata\\accounts.json";
 
 	@Override
-	public Result addAccount(AccountDTO accountDTO) {
-		try {
-			String json = JsonWriter.objectToJson(accountDTO);
-			System.out.println(json);
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-			return new Result(false, e.getMessage());
-		}
+	public Result addAccount(AccountDTO arg0) {
+		JSONParser parser = new JSONParser();
 
-		return new Result(true, "");
+		try {
+			File fileCheck = new File(path);
+			if(fileCheck.length() > 0){
+				Object obj = parser.parse(new FileReader(path));
+				
+				JSONObject jsonObject = (JSONObject) obj;
+				JSONArray jsonArray = (JSONArray) jsonObject.get("accounts");
+	
+				JSONObject account = new JSONObject();
+				account.put("gameID", arg0.getGameID());
+				Account a = arg0.getAccount();
+				account.put("account", JsonWriter.toJson(a));
+				
+				jsonArray.add(account);
+				
+				FileWriter file = new FileWriter(path);
+				file.write(obj.toString());;
+				file.flush();
+				file.close();
+			} else {
+				JSONObject jsonObject = new JSONObject();
+				JSONArray jsonArray = new JSONArray();
+	
+				JSONObject account = new JSONObject();
+				account.put("gameID", arg0.getGameID());
+				Account a = arg0.getAccount();
+				account.put("account", JsonWriter.toJson(a));
+				
+				jsonArray.add(account);
+				jsonObject.put("accounts", jsonArray);
+				
+				FileWriter file = new FileWriter(path);
+				file.write(jsonObject.toString());;
+				file.flush();
+				file.close();
+				
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	@Override
 	public Result clearData() {
-		// TODO Auto-generated method stub
+		try {
+			FileWriter file = new FileWriter(path);
+			file.write("");
+			file.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public Set<AccountDTO> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		JSONParser parser = new JSONParser();
+		Set<AccountDTO> accounts = new HashSet<>();
+		
+		try {
+			File fileCheck = new File(path);
+			if(fileCheck.length() == 0) return accounts;
+			Object obj = parser.parse(new FileReader(path));
+			
+			JSONObject jsonObject = (JSONObject) obj;
+			JSONArray jsonArray = (JSONArray) jsonObject.get("accounts");
+			Iterator i = jsonArray.iterator();
+			
+			while(i.hasNext()){
+				JSONObject accountObj = (JSONObject) i.next();
+				int gameID = (int) (long) accountObj.get("gameID");
+				String accountStr = (String) accountObj.get("account");
+				Account account = (Account) JsonReader.jsonToJava(accountStr);
+				AccountDTO dto = new AccountDTO();
+				dto.setGameID(gameID);
+				dto.setAccount(account);
+				
+				accounts.add(dto);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		return accounts;
 	}
 
 	@Override
 	public AccountDTO selectByAuth(String arg0) {
-		// TODO Auto-generated method stub
+		JSONParser parser = new JSONParser();
+
+		try {
+			File fileCheck = new File(path);
+			if(fileCheck.length() == 0) return null;
+
+			Object obj = parser.parse(new FileReader(path));
+			
+			JSONObject jsonObject = (JSONObject) obj;
+			JSONArray jsonArray = (JSONArray) jsonObject.get("accounts");
+			Iterator i = jsonArray.iterator();
+			
+			while(i.hasNext()){
+				JSONObject accountObj = (JSONObject) i.next();
+				int gameID = (int) (long) accountObj.get("gameID");
+				String accountStr = (String) accountObj.get("account");
+				Account account = (Account) JsonReader.jsonToJava(accountStr);
+				if(arg0.equals(account.getAuthentication())){
+					AccountDTO dto = new AccountDTO();
+					dto.setGameID(gameID);
+					dto.setAccount(account);
+					return dto;
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public AccountDTO selectByUserName(String arg0) {
-		// TODO Auto-generated method stub
+		JSONParser parser = new JSONParser();
+
+		try {
+			File fileCheck = new File(path);
+			if(fileCheck.length() == 0) return null;
+
+			Object obj = parser.parse(new FileReader(path));
+			
+			JSONObject jsonObject = (JSONObject) obj;
+			JSONArray jsonArray = (JSONArray) jsonObject.get("accounts");
+			Iterator i = jsonArray.iterator();
+			
+			while(i.hasNext()){
+				JSONObject accountObj = (JSONObject) i.next();
+				int gameID = (int) (long) accountObj.get("gameID");
+				String accountStr = (String) accountObj.get("account");
+				Account account = (Account) JsonReader.jsonToJava(accountStr);
+				if(arg0.equals(account.getUsername())){
+					AccountDTO dto = new AccountDTO();
+					dto.setGameID(gameID);
+					dto.setAccount(account);
+					return dto;
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
